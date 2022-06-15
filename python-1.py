a3f69478c5efee5654a268c87afbbb56fd66b9c1
kdunaev-a10 <ciberkot@gmail.com>
2022-06-16 00:01:49 +0200

    added input and output

--- python-1.py
+++ python-1.py
@@ -77,4 +77,4 @@ currency = 'EUR'
 rate = 86.93
 
 print('the currency {2} on date {1} had value {2:.3f}'.format(currency,cur_date,rate) )
-print(f'the currency {currency} on date {cur_date} had value {rate:.3f}'.format(currency,cur_date,rate) )
+print(f'the currency {currency} on date {cur_date} had value {rate:.3f}' )
