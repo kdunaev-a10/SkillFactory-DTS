b8de1943212ba7e733cca5ad8ffd66a7c283696b
kdunaev-a10 <ciberkot@gmail.com>
2022-06-19 00:19:28 +0200

    added input and output

--- python-2.py
+++ python-2.py
@@ -233,3 +233,179 @@ print(s)
 #4.3
 s3 = set('wow thats cool')
 print(s3)
+
+#4.4
+s3 = set("abcde")
+s3.add("hello")
+print(s3)
+s3 = set("abcde")
+s3.update("hello")
+print(s3)
+
+#4.5
+s3 = set("abcde")
+s3.remove("d")
+print(s3)
+
+#4.6
+s3 = set("abcdef")
+s3.discard("g")
+#s3.remove("g")
+print(s3)
+
+#4.7
+cluster1 = {"item1", "item2", "item3", "item4"}
+cluster2 = {"item2", "item3", "item5", "item7"}
+print(cluster1.union(cluster2))
+
+#4.8
+s3 = set("abcde")
+name = set("bad boy")
+print(s3.union(name))
+
+#4.9
+s3 = set(range(0,11))
+name = set([1,9,4,8])
+print(s3.union(name))
+
+#4.10
+s3 = set("abcde")
+name = set("bad boy")
+print(s3.intersection(name))
+
+#4.11
+s3 = set(range(0,11))
+name = set([1,9,4,8])
+print(s3.intersection(name))
+
+#4.12
+s3 = set("abcde")
+name = set("bad boy")
+print(s3.difference(name))
+
+#4.13
+s3 = set(range(0,11))
+name = set([1,9,4,8])
+print(s3.difference(name))
+
+s3 = set(range(0,11))
+name = set([1,9,4,8])
+print(name.issubset(s3))
+
+list6 = [3, 1, -10, 5, 11, 20, 1, -10]
+list7 = list6.copy()
+#list_result = (list6.sort().reverse())
+list6.sort(reverse=True)
+#list = list6.reverse()
+print(list6)
+print(type(list))
+
+
+#5.2
+num1 = 3
+num2 = 3.5
+
+print(float(num1) + num2)
+
+#5.3
+int1 = 1
+print(str(int1))
+
+#5.4
+str1 = 'I have '
+int1 = 5
+str2 = ' brothers'
+print(str1 + str(int1) + str2)
+
+#5.6
+s = 'hello work'
+#int1 = int(s)
+
+#5.7
+s = '10'
+int1 = int(s)
+print(int1*3)
+
+#5.8
+s = '11'
+int1 = float(s)
+print(int1*3)
+
+s = "50.4"
+#int1 = int(s)
+print(int1)
+
+#####
+#lists
+#####
+
+dictionary = {"Anne": 15, "Sam": 30, "Marie": 22}
+only_keys = dictionary.keys()
+only_keys = list(only_keys)
+# dict_keys(["Anne", "Sam", "Marie"])
+only_keys = only_keys + ["Tom", "Curt"]
+
+d = {("Amanda", 22, "NY"): "3rd place"}
+input_key = tuple(["Collin", 23, "LA"])
+input_value = "2nd place"
+d[input_key] = input_value
+print(d)
+
+#5.10
+tpl1 = tuple(range(1,6))
+print(tpl1)
+#5.11
+#tpl1 = tuple("abcde")
+tpl1 = ("a", "b", "c", "d", "e")
+list1 = list(tpl1)
+print(list1)
+
+#6.1
+l = []
+l.append(1)
+l.append(2)
+l.append(3)
+print(l)
+
+#6.2
+l2 = list(range(-5,15,2))
+l3 = [1,2,3,4]
+l3.extend(l2)
+print(l3)
+
+#6.3
+l6 = [3, 1, -10, 5, 11, 20, 1, -10]
+l7 = l6.copy()
+l6.sort()
+l6.reverse()
+res = l6
+print(l7, res)
+
+#6.4
+l = [10,15]
+tpl1 = tuple(l)
+dict1 = {}
+dict1[tpl1] = "hello"
+print(dict1)
+
+#6.5
+dict1 = {"name" : "unknown"}
+dict2 = {"name" : "Tom"}
+print(dict1, dict2, dict1.keys(), dict1.values())
+dict1["name1"] = "test"
+print(list(dict1))
+
+#6.6
+s1 = set("hello")
+s2 = set(["w", "o", "w"])
+print(s1.union(s2), s1.intersection(s2), s1.difference(s2))
+
+#6.7
+s1 = 'age is '
+int1 = 15
+print(s1 + str(int1))
+
+s2=s1.split(" ")
+print(s2)
+s3 = "".join(s1.split(" "))
+print(s3)
