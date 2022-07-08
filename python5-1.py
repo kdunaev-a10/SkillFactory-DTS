9276b8e174040ea42e3fefd68cc0afcd7ab31bcc
kdunaev-a10 <ciberkot@gmail.com>
2022-07-08 12:05:24 +0200

    continue the module

--- python5-1.py
+++ python5-1.py
@@ -139,20 +139,146 @@ print(group2)
 group3 = add_mark_correct('Kuznetsova', 5)
 print(group3)
 
-
+#3.1
+print("##3.1")
 def add_mark(name, mark, journal=None):
     # Добавьте здесь проверку аргумента mark
     if  mark not in [2,3,4,5]:
         raise ValueError("Invalid Mark!")
     if journal is None:
         journal = {}
-
     journal[name] = mark
     return journal
-
 # попытка вызвать функцию с некорректными аргументами
 try:
     print(add_mark('Ivanov', 6))
 # должна завершиться с ошибкой ValueError, которую мы выведем в блоке except
 except ValueError as e:
     print(e)
+
+#4
+def root(value, n=2, verbose=False):
+    # Как мы уже выяснили, чтобы посчитать
+    # корень степени n из числа, можно возвести это число
+    # в степень 1/n
+    result = value ** (1/n)
+    if verbose:
+        print('Root of power', n, 'from',
+              value, 'equals', result)
+    return result
+
+# Посчитаем корень 3-ей степени (кубический корень) из 27
+print(root(25))
+print(root(27,3))
+print(root(81,4,True))
+
+print(root(25, verbose=True))
+#print(root(verbose=True, 25))
+print(root(81,verbose=True, n=4))
+print(root(verbose=True, n=4, value=81))
+
+print(25, 125, 625, sep=', ')
+
+print("Shopping list:", end=' ')
+print("bread", "butter", "eggs")
+
+#4.1
+print("#4.1")
+
+print("Shopping list:", end=' ')
+print("bread", "butter", "eggs", sep=", ")
+
+#5
+print("##5")
+def mean(*args):
+    # Среднее значение — это сумма всех значений,
+    # делённая на число этих значений
+    # Функция sum — встроенная, она возвращает
+    # сумму чисел
+    print(isinstance(args, tuple))
+    print(args)
+    result = sum(args)/len(args)
+    return result
+
+print(mean(2,3,4,5,6))
+
+def mean_mark(name, *marks):
+    result = sum(marks) / len(marks)
+    # Не возвращаем результат, а печатаем его
+    print(name+':', result)
+mean_mark("Ivanov", 5, 5, 5, 4)
+mean_mark("Petrov", 5, 3, 5, 4)
+
+#Wrong
+def mean_mark_wrong(*marks, name):
+    result = sum(marks) / len(marks)
+    print(name + ':', result)
+
+mean_mark_wrong(5, 5, 5, 4, name="Ivanov")
+
+#5.1
+print("##5.1")
+
+def mult(*args):
+    result = 1
+    for elm in args:
+        result *= elm
+    return result
+
+print(mult(3,5,10))
+
+#5.2
+print("##5.2")
+langs = ['Python', 'SQL', 'Machine Learning', 'Statistics']
+print(langs)
+print(*langs, sep=", ")
+
+#5.3
+print("##5.3")
+marks = [4,5,5,5,5,3,4,4,5,4,5]
+def mean_mark(name, *marks):
+    result = sum(marks) / len(marks)
+    # Не возвращаем результат, а печатаем его
+    print(name+':', result)
+
+mean_mark("Kuznetsov", *marks)
+
+#5.4
+print("##5.4")
+def schedule(**kwargs):
+    # kwargs — это словарь, проверим это с помощью isinstance:
+    print(isinstance(kwargs, dict))
+    # Напечатаем объект kwargs
+    print("Weekly schedule")
+    for key in kwargs:
+        print(key, kwargs[key], sep=", ")
+
+
+schedule(monday='Python', tuesday='SQL', friday='ML')
+lessons = {
+    'Wednesday': 'Maths',
+    'Thursday': 'SQL',
+    'Friday': 'Statistics'
+}
+schedule(**lessons)
+
+def print_args(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+print_args(1,4,5,7, name='Ivanov', age=19, city='Moscow')
+
+how = {'sep': ', ', 'end': '; '}
+list1 = [1,4,6,8]
+list2 = [12, 45, 56, 190, 111]
+list3 = ['Python', 'Functions']
+print(list1,list2,list3)
+print(*list1, **how)
+print(*list2, **how)
+print(*list3, **how)
+print()
+
+print([12, 23, 45],[34, 56, 78], sep='. ', end=';')
+print()
+#6
+print("##6")
