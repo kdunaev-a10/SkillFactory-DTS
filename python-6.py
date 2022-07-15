7556195d9a9fc49472475540d1f11fb4ee72ad02
kdunaev-a10 <ciberkot@gmail.com>
2022-07-15 23:57:49 +0200

    6 the module

--- python-6.py
+++ python-6.py
@@ -232,3 +232,144 @@ def division(a, b):
 
 print(division(1, 0))
 print(division(1, 1))
+
+#4.4
+print("###4.4")
+# Напишите функцию lucky_ticket(), которая проверяет, является ли билетик счастливым.
+# Памятка: билетик счастливый, если сумма первых трех цифр равна сумме последних трех цифр.
+# На вход функция получает шестизначное число.
+
+def lucky_ticket(ticket_number):
+    first_three_num_str = str(ticket_number // 1000)
+    last_three_num_str = str(ticket_number % 1000)
+    print(first_three_num_str,last_three_num_str)
+
+    str_number = str(ticket_number)
+    first_3 = 0
+    second_3 = 0
+    for s in str_number[:3]:
+        first_3 += int(s)
+    for s in str_number[3:]:
+        second_3 += int(s)
+    print(first_3,second_3)
+    return first_3 == second_3
+
+print(lucky_ticket(111111))
+print(lucky_ticket(123456))
+
+#4.5
+print("###4.5")
+
+# Напишите функцию def fib_number(), которая получается на вход некоторое
+# число n и выводит n-e число Фибоначчи.
+# Задачу можно решить как с помощью цикла for, так и с помощью цикла while
+# Примечание: числа Фибоначчи определяются так
+# ```
+# a0 = 0, a1 = 1, a2 = a1 + a0 = 1, an = a_n-1 + a_n-2
+# ```
+# Примечание: в модуле по функциям уже было задание на вычисление чисел Фибоначчи
+# с помощью рекурсивных функций. Здесь необходимо реализовать те же вычисления,
+# но без использования рекурсии.
+
+def fib_number1(n):
+    fib_n = 0
+    fib_n_1 = 1
+    temp = 0
+    for counter in range(1,n+1):
+        print(fib_n, fib_n_1, temp)
+        fib_n += fib_n_1
+        fib_n_1 = temp
+        temp = fib_n
+
+    return fib_n
+
+def fib_number(n):
+    fib_n = 0
+    fib_n_1 = 1
+    fib_n_2 = 0
+    #counter = 0
+    for counter in range(2,n+1):
+        fib_n = fib_n_2 + fib_n_1
+        fib_n_2 = fib_n_1
+        fib_n_1 = fib_n
+    if n == 1:
+        return 1
+    return fib_n
+
+print(fib_number1(4))
+
+#4.6
+print("###4.6")
+# Напишите функцию def even_numbers_in_matrix(),
+# которая получает на вход матрицу (список из списков)
+# и возвращает количество четных чисел в ней.
+
+matrix_example = [
+          [0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]
+]
+matrix_example = [[117, 56, 38, 11, 0], [1, 4, 3, 2, 3], [-4, 5, 6, -10, 2]]
+
+def even_numbers_in_matrix(matrix):
+    even_num = 0
+    for i in range(len(matrix)):
+        print(matrix[i])
+        for j in range(len(matrix[i])):
+            if matrix[i][j] % 2 == 0:
+                even_num += 1
+    return even_num
+
+def even_numbers_in_matrix(matrix):
+    even_num = 0
+    for line in matrix:
+        for elm in line:
+            if elm % 2 == 0:
+                even_num += 1
+    return even_num
+
+print(even_numbers_in_matrix(matrix_example))
+
+
+#4.7
+print("###4.7")
+# Напишите функцию def matrix_sum(), которая получает на вход две матрицы
+# и возвращает их сумму.
+# Примечание: чтобы найти сумму двух матриц, нужно просуммировать
+# их соответствующие элементы. Но перед этим необходимо проверить, что размеры
+# матриц одинаковы (одинаковое количество столбцов и одинаковое количество строк)
+# Например:
+
+# 1 2 3   2 3 4   3 5 7
+# 2 3 4 + 4 5 6 = 6 8 10
+# 5 6 7   4 3 2   9 9 9
+
+matrix_example = [
+          [1, 5, 4],
+          [4, 2, -2],
+          [7, 65, 88]
+]
+matrix_example1 = [
+          [1, 5, 4, 6],
+          [4, 2, -2, 8],
+          [7, 65, 88, 9]
+]
+
+def matrix_sum(matrix1, matrix2):
+    num_row_1 = len(matrix1)
+    num_row_2 = len(matrix2)
+    elm_count_1 = len(matrix1[0])
+    elm_count_2 = len(matrix2[0])
+    print(num_row_1, num_row_2,elm_count_1, elm_count_2)
+
+    if (num_row_1 != num_row_2) or (elm_count_1 != elm_count_2):
+        print('Error! Matrices dimensions are different!')
+        return None
+    matrix_s = matrix1.copy()
+
+    for i,line in enumerate(matrix2):
+        for j,elm in enumerate(line):
+            matrix_s[i][j] += elm
+    return matrix_s
+
+print(matrix_sum(matrix_example, matrix_example))
