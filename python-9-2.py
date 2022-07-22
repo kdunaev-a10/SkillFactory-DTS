6d7854ec6ddc751f898326662e611b3a0e937940
kdunaev-a10 <ciberkot@gmail.com>
2022-07-22 23:56:23 +0200

    start 9-2

---
+++ python-9-2.py
@@ -0,0 +1,14 @@
+# Импортируем объект Counter из модуля collections
+from calendar import day_abbr
+from collections import Counter
+from operator import le
+from pydoc import ModuleScanner
+from collections import defaultdict
+from collections import deque
+from collections import OrderedDict
+from matplotlib.cbook import print_cycles
+import numpy as np
+
+a = np.int8(25)
+print(a)
+print(type(a))
