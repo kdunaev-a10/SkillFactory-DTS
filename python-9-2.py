fca7be10351ae197d91c1d9c08ca927f4f02f265
kdunaev-a10 <ciberkot@gmail.com>
2022-07-29 00:11:30 +0200

    6 the module

--- python-9-2.py
+++ python-9-2.py
@@ -250,6 +250,206 @@ print(nd_array[1:4,2:5])
 print('')
 print(nd_array[::-1,-1])
 
+####
+print('####')
 
-###7.3
-print('###7.3')
+arr = np.array([23,12,45,12,23,4,15,3])
+arr_new = np.sort(arr, )
+print(arr)
+# [23 12 45 12 23  4 15  3]
+print(arr_new)
+# [ 3  4 12 12 15 23 23 45]
+
+arr = np.array([23,12,45,12,23,4,15,3])
+print(arr.sort())
+# None
+print(arr)
+
+print('####')
+print('roots')
+data = np.array([4, 9, -4, 3, -5])
+roots = np.sqrt(data)
+print(roots)
+
+print(None is None)
+# True
+print(np.nan is np.nan)
+# True
+print(np.nan is None)
+# False
+print(sum(roots))
+print(np.isnan(roots))
+print(sum(np.isnan(roots)))
+
+print(roots[np.isnan(roots)])
+roots[np.isnan(roots)] = 0
+print(roots)
+print(sum(roots))
+
+
+###7.5
+print('###7.5')
+mystery = np.array([ 12279, -26024,  28745,      0,  31244,  -2365,  -6974,  -9212,      0, -17722,
+                     16132,  25933,      0, -16431,  29810])
+nans_index = np.isnan(mystery)
+# В переменную n_nan сохраните число пропущенных значений
+n_nan = sum(nans_index)
+# Заполните пропущенные значения в массиве mystery нулями
+mystery[np.isnan(mystery)] = 0
+print(mystery)
+print(mystery.dtype)
+
+# Поменяйте тип данных в массиве mystery на int32
+mystery = np.int32(mystery)
+print(mystery)
+
+
+# Отсортируйте значения в массиве по возрастанию и сохраните
+# результат в переменную array
+array = np.sort(mystery)
+
+# Сохраните в массив table двухмерный массив, полученный из массива array
+# В нём должно быть 5 строк и 3 столбца. Причём порядок заполнения должен быть
+# по столбцам! Например, 1, 2, 3, 4 -> 1    3
+#                                      2    4
+table = array.reshape((5,3),order='F')
+print(table)
+#  Сохраните в переменную col средний столбец из table
+col = table[:,1]
+print(col)
+
+#####
+#vektors
+print()
+print()
+
+vec1 = np.array([2, 4, 7, 2.5])
+vec2 = np.array([12, 6, 3.6, 13])
+print(vec1 + vec2)
+
+list1 = [2, 4, 7, 2.5]
+list2 = [12, 6, 3.6, 13]
+print(list1 + list2)
+print([x + y for x, y in zip(list1,list2)])
+
+vec1 = np.array([2, 4, 7, 2.5])
+vec2 = np.array([12, 6, 3.6])
+#print(vec1 * vec2)
+# ValueError: operands could not be broadcast together with shapes (4,) (3,)
+
+vec = np.arange(5)
+print(vec * 10)
+# array([ 0, 10, 20, 30, 40])
+print(vec ** 2)
+
+
+vec1 = np.array([2, 4, 7, 2.5])
+vec2 = np.array([12, 6, 3.6, 13])
+arr = vec1 > vec2
+print(type(arr))
+
+
+####
+vec = np.array([3, 4])
+length = np.sqrt(np.sum(vec ** 2))
+print(length)
+
+length = np.linalg.norm(vec)
+print(length)
+
+vec1 = np.array([0, 3, 5])
+vec2 = np.array([12, 4, 7])
+dist = np.sqrt(np.sum((vec1 - vec2) ** 2))
+print(dist)
+
+dist = np.linalg.norm(vec1 - vec2)
+print(dist)
+
+vec1 = np.arange(1, 6)
+vec2 = np.linspace(10, 20, 5)
+dot = np.sum(vec1 * vec2)
+print(dot)
+
+dot = np.dot(vec1,vec2)
+print(dot)
+
+x = np.array([25, 0])
+y = np.array([0, 10])
+print("ortogonal: ", np.dot(x, y))
+
+
+###8.4
+print('##8.4')
+
+a = np.array([23, 34, 27])
+b = np.array([-54, 1,  46])
+c = np.array([46, 68, 54])
+
+print(np.linalg.norm(a + b), np.linalg.norm(a) + np.linalg.norm(b))
+print(np.linalg.norm(a + c), np.linalg.norm(a) + np.linalg.norm(c))
+print(np.linalg.norm(c + b), np.linalg.norm(c) + np.linalg.norm(b))
+
+###8.5
+print('##8.5')
+print(np.linalg.norm(a - b))
+print(np.linalg.norm(a - c))
+print(np.linalg.norm(c - b))
+
+
+
+###8.6
+print('##8.6')
+
+print(np.dot(a,b))
+print(np.dot(a,c))
+print(np.dot(c,b))
+
+###
+print('##')
+vec = np.array([2,7,18,28,18,1,8,4])
+print(vec.min(), np.min(vec))
+# 1
+print(np.max(vec), vec.max())
+# 28
+print(vec.mean())
+
+mystery = np.array([2,7,18,28,18,1,8,4])
+print(np.min(mystery))
+print(np.mean(mystery))
+print(np.median(mystery))
+print(np.std(mystery))
+
+
+####9
+print()
+print()
+print('###9')
+print(np.random.rand())
+print(np.random.rand() * 100)
+print(np.random.rand(5))
+print(np.random.rand(2,3))
+#print(np.random.rand(2,3,2,4,5,6))
+
+shape = (3, 4)
+#print(np.random.rand(shape))
+# TypeError: 'tuple' object cannot be interpreted as an integer
+print(np.random.rand(*shape))
+
+#Sample random
+shape = (2, 3)
+print(np.random.sample(shape))
+
+#uniform
+print()
+print('##UNIFORM')
+print(np.random.uniform(low=0.0, high=1.0, size=None))
+print(np.random.uniform(10,50))
+print(np.random.uniform(0.3,0.6, size = 5))
+
+print(np.random.uniform(300,699, size = (2,3)))
+
+#INT
+print()
+print('##INT')
+print(np.random.randint(6,12, size = (2,3)))
+print(np.random.randint(10, size = (2,3)))
