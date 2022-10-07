4a9d4581776c5b751af021da7cab8f250f83a913
kdunaev-a10 <ciberkot@gmail.com>
2022-10-07 15:22:59 +0200

    pushed module12

--- module-python/module15/module15-1.py
+++ module-python/module15/module15-1.py
@@ -1,6 +1,8 @@
 class DepartmentReport():
-    def __init__(self):  
+    def __init__(self, company):  
         self.revenues = []  
+        self.company = company 
+        
     def add_revenue(self, amount):
         """
         Метод для добавления выручки отдела в список revenues.
@@ -15,16 +17,57 @@ class DepartmentReport():
         """
         Метод возвращает среднюю выручку по отделам.
         """
-        return sum(self.revenues)  / len(self.revenues)
+        average = round(sum(self.revenues)  / len(self.revenues))
+        return 'Average department revenue for {}: {}'.format(self.company,average)
       
     def print_report(self):  
         """_summary_
         """
-        print("Total sales:", self.average_revenue()) 
+        print("Average department revenue for {}: {}".format(self.company, self.average_revenue())) 
 
 
-report = DepartmentReport()
+report = DepartmentReport('test')
 report.add_revenue(1_000_000)
 report.add_revenue(400_000)
 print(report.revenues)
 print(report.average_revenue())
+
+report1 = DepartmentReport("Danon")
+report1.add_revenue(1_000_000)
+report1.add_revenue(400_000)
+print(report1.average_revenue())
+
+
+
+class SalesReport():  
+    def __init__(self, employee_name):  
+        self.deals = []  
+        self.employee_name = employee_name  
+      
+    def add_deal(self, company, amount):   
+        self.deals.append({'company': company, 'amount': amount})  
+          
+    def total_amount(self):  
+        #return sum([deal['amount'] for deal in self.deals])  
+        return [deal['amount'] for deal in self.deals]
+      
+    def average_deal(self):  
+        #return self.total_amount()/len(self.deals)  
+        return 100
+      
+    def all_companies(self):  
+        return list(set([deal['company'] for deal in self.deals]))  
+      
+    def print_report(self):  
+        print("Employee: ", self.employee_name)  
+        print("Total sales:", self.total_amount())  
+        print("Average sales:", self.average_deal())  
+        print("Companies:", self.all_companies())  
+
+report = SalesReport("Ivan Semenov")  
+  
+report.add_deal("PepsiCo", 120_000)  
+report.add_deal("SkyEng", 250_000)  
+report.add_deal("PepsiCo", 20_000)  
+  
+report.print_report()  
