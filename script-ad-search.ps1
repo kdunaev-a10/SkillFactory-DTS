89ae5ba2c749c00e24e7ee580c0cbb3d07e12a70
kdunaev-a10 <ciberkot@gmail.com>
2023-03-03 00:21:47 +0100

    pushed ml-1

---
+++ script-ad-search.ps1
@@ -0,0 +1,18 @@
+$search = New-Object DirectoryServices.DirectorySearcher([ADSI]"")
+$search.filter = "(servicePrincipalName=*)"
+$results = $search.Findall()
+foreach($result in $results) {
+    $userEntry = $result.GetDirectoryEntry()
+    Write-Host "Object-Name = " $userEntry.name -BackgroundColor "yellow" -ForegroundColor "black"
+    Write-Host "DN      =     " $userEntry.distinguishedName
+    Write-Host "Object Cat. = " $userEntry.objectCategory
+    Write-Host "servicePrincipalNames"
+    $i=1
+    foreach($SPN in $userEntry.servicePrincipalName) {
+        Write-Host "SPN(" $i ")    =    " $SPN
+        $i+=1
+    }
+    Write-Host ""
+}
+
+#Get-ADComputer -Filter * -Properties * | FT name, ipv4Address
